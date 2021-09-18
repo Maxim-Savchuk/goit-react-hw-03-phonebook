2433d22ed4fb11911b96874acaff75b50ca46eed
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-09-18 17:45:17 +0300

    Finished-phonebook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,12 +10,7 @@ import { Container, PhonebookTitle, ContactsTitle } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -42,6 +37,23 @@ export class App extends Component {
       filter: e.currentTarget.value,
     });
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts))
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
